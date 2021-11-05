94376f4e831f507134b130fa35d3676179f551a7
dinojetpilot <jason.millet@gmail.com>
2021-11-04 19:08:05 -0500

    starting loops unit

--- reference/objects_cheatsheet.js
+++ reference/objects_cheatsheet.js
@@ -6,6 +6,7 @@ const dog = {
     age: 7
 }
 // All keys will be turned into strings!
+// i.e. "name" is the key, and "Rusty" is the value
 
 // To retrieve a value:
 dog.age; //7
@@ -14,3 +15,52 @@ dog["age"]; //7
 //updating values
 dog.breed = "mutt";
 dog["age"] = 8;
+
+// Dot notation is rigid, but square brakets allow for dynamic elements.
+// For example year[birthYear] looks for the value of that variable, then it will put that value inplace of the variable and search for a key with that value
+
+
+// Example use from Colt's Bootcamp
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+
+// An array can contain obects...
+const shoppingCart = [
+    {
+        product: 'milk',
+        price: 2.99,
+        quantity: 1
+    },
+    {
+        product: 'eggs',
+        price: 3.99,
+        quantity: 2
+    },
+    {
+        product: 'bread',
+        price: 2.49,
+        quantity: 2
+    }
+]
+// To access an element:
+shoppingCart[1].price // returns 3.99
+
+
+// ...Or an object can have arrays and objects.
+const student = {
+    firstName: John,
+    lastName: Doe,
+    classes: ['English', 'Math'],
+    exams: {
+        midterm: 90,
+        final: 88
+    }
+}
---
+++ web-dev-bootcamp-files/section-19-loops/ForLoops/app.js
@@ -0,0 +1,55 @@
+// ======================================
+// WITHOUT ANY LOOPS...LOTS OF REPETITION
+// ======================================
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// ======================================
+// OUR FIRST FOR LOOP
+// ======================================
+
+for (let num = 1; num <= 10; num += 1) {
+    console.log(num);
+}
+
+// ======================================
+// MORE EXAMPLES OF FOR LOOPS!
+// ======================================
+
+// prints even numbers up to 20. 
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i)
+}
+
+// counts down from 100 by tens.
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+// prints 10 to 1000 in increments of 10.
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
+
+// DO NOT RUN THIS LOOP
+// INFINITE LOOP!! BAD!!
+// ================================
+// ================================
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i)
+// }
+// ================================
+// ================================
+
+
+
+
+
