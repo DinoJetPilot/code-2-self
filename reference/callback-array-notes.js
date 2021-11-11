ec59be1b2089ab013a17fe7abea983ba51da165c
dinojetpilot <jason.millet@gmail.com>
2021-11-10 23:30:32 -0600

    Working on card challenge

--- reference/callback-array-notes.js
+++ reference/callback-array-notes.js
@@ -184,3 +184,69 @@ movies
 
 const badMovies = movies.filter(m => m.score < 70);
 const recentMovies = movies.filter(m => m.year > 1999);
+
+
+// ======================================================
+// "every" check whether ALL elements in an array pass a provided function. It returns a Boolean value.
+//
+// "some" is similar to "every" but returns true if ANY array element passes the function test.
+//
+
+const exams = [80, 98, 92, 78, 77, 90, 89, 84, 81, 77]
+
+const didAllPass = exams.every(score => score >= 75);
+const didAnyMaster = exams.some(score => score >= 90);
+
+
+// ======================================================
+// "reduce" executes a reducing function on each element of the array, resulting in a single value.
+//
+
+// value "accumulator" and "currentValue" can be named anything, but the first value is what's being reduced. The second value is each individual element in the array.
+// For example:
+[3, 5, 7, 9, 11].reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+});
+// CALLBACK    - accumulator - currentValue - return value
+// first call     3             5               8
+// second call    8             7               15
+// third call     15            9               24
+//  fourth call   24            11              35
+
+
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+// Usin a "for...of" array loop:
+// let total = 0;
+// for (let price of prices) {
+//     total += price
+// };
+// console.log(total);
+
+// Same as above, but using "reduce" funtion:
+// const total = prices.reduce((total, price) => {
+//     return total + price
+// });
+
+// Same again, condensed more:
+const total = prices.reduce((total, price) => total + price);
+// Can also add a second argument as a starting value. In the example below, 100 is the starting value, then all values are combined and added to that.
+// const total = prices.reduce((total, price) => total + price, 100);
+
+// Trying to find the smallest element in the array:
+const minPrice = prices.reduce((minPrice, price) => {
+    if (price < min) {
+        return price;
+    } else {
+        return min;
+    }
+});
+
+// Finding the highest rated movie from array earlier:
+const highestRating = movies.reduce((highest, mov) => {
+    if (highest.score > mov.score) {
+        return highest;
+    } else {
+        return mov;
+    }
+});
