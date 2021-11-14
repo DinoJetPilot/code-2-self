b08d6c4155e3570550709061ba1b0a724974c573
dinojetpilot <jason.millet@gmail.com>
2021-11-14 09:01:53 -0600

    Notes revisions

---
+++ reference/destructuring-notes.js
@@ -0,0 +1,3 @@
+// =========================================
+// Destructuring allows you to unpack (destructure) the values from arrays or take the properties from bojects to place in distinct variables.
+
