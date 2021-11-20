15cdc0c071e261814708e0d3d60d8924abfc3fb9
dinojetpilot <jason.millet@gmail.com>
2021-11-19 22:06:20 -0600

    Promises notes

--- reference/es6-script-basics.js
+++ reference/es6-script-basics.js
@@ -10,4 +10,28 @@
 // modules
 
 // =========================================
-// 
+// A "Promise" is a constructor function that will work asynchronously, by either fulfilling the promise or failing to do so. It requires the "new" keyword and has two parameters, "resolve" and "reject". The method(s) within will determine the outcome of the promise.
+
+const myEndlessPromise = new Promise((resolve, reject) => {
+    // some method(s)
+});
+
+// Promises have three states, "pending"(the default state), "resolve", and "rejected". Within the promise, "resolve" is what fulfills a promise, "reject" is what causes it to fail.
+
+const myBetterPromise = new Promise((resolve, reject) => {
+    if (someObjectVariable) {
+        resolve("Promise was fulfilled");
+    } else {
+        reject("Promise was rejected");
+    }
+});
+
+// Promises tend to be most used with someting like server requests or other considerations that that an unknown amount of time to complete.
+
+// You can add the "then" method executes the code immediately after the promise is fulfilled. "result" is from the argument given in the "resolve" method of the promise.
+
+myPromise.then(result => { });
+
+// A "catch" is a methos like the "then", except it's used for when a promise is rejected. It is also executed immediately after the promise is rejected. Similarly, "error" is from the "reject" argument in the promise.
+
+myPromise.catch(error => { });
