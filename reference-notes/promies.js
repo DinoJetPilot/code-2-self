2f21fe761e2956294f08b94bc4c6a81146863989
dinojetpilot <jason.millet@gmail.com>
2021-12-09 21:42:37 -0600

    Finished basic promis notes

--- reference-notes/promies.js
+++ reference-notes/promies.js
@@ -41,6 +41,7 @@ fakeRequestCallback('books.com/page1', // url
         console.log("ERROR!!!", err)
     })
 
+// =========================================
 
 // THE PROMISE VERSION 
 const fakeRequestPromise = (url) => {
@@ -56,3 +57,72 @@ const fakeRequestPromise = (url) => {
         }, delay)
     })
 }
+
+// If you run the function with an example, then look in the dev tools in the console, it will list the object like this at first:
+// > Promise {<pending>}
+// If you dropdown the arrow, it that PromiseStatus (i.e. pending) and the PromiseValue (i.e. "undefined" in this case)
+
+// But after completion, it shows:
+// > Promise {<resolved>} (or something similar)
+// Once completed whether resolved or rejected, the PromisValue updates to the code in that value.
+
+// Doesn't need to be as a variable, but many people use it like that.
+// const requestInfo = fakeRequestPromise("fake.website.com/apis/me");
+
+// In order to attach a callback to the resolve value using the then() method. To pass a method to the rejected value, use .catch()
+// requestInfo.then(() => {
+//     console.log("Success!")
+// }).catch(() => {
+//     console.log("Failed!")
+// });
+
+// An easier to read (but still basic and nested) version of the code above can be formatted like:
+// fakeRequestPromise("fake.website.com/apis/info-1")
+// requestInfo
+//     .then(() => {
+//         console.log("Promise resolved!")
+//         // Can add the next callback by adding a new url:
+//         fakeRequestPromise("fake.website.com/apis/info-2")
+//             .then(() => {
+//                 console.log("Promise resolved, again!!")
+//                 // Can add the next callback by adding a new url:
+//                 fakeRequestPromise("fake.website.com/apis/info-3")
+//                     .then(() => {
+//                         console.log("Promise resolved, yet again!!!")
+//                     })
+//                     .catch(() => {
+//                         console.log("Promise rejected on the third try.")
+//                     })
+//             })
+//             .catch(() => {
+//                 console.log("Promise rejected the second time...")
+//             })
+//     })
+//     .catch(() => {
+//         console.log("Promise rejected!")
+//     });
+
+// An even better way to write the same promise from above:
+fakeRequestPromise("fake.website.com/apis/info-1")
+    // The argument passed, "data" in this case uses the code from the promise stored in the variable/function at the top of this code.
+    .then((data) => {
+        console.log("Promise resolved!")
+        console.log(data) // Returns `Here is your fake data from ${url}`
+        // Can add the next callback by adding a new url with a return:
+        return fakeRequestPromise("fake.website.com/apis/info-2")
+        // This allows you to chain on the .then() after this promise
+    })
+    .then((data) => {
+        console.log("Promise resolved, again!!")
+        console.log(data)
+        return fakeRequestPromise("fake.website.com/apis/info-3")
+    })
+    .then((data) => {
+        console.log("Promis resolved the third time!!!")
+        console.log(data)
+    })
+    // You can use a single .catch() for all of the promises chained:
+    .catch((err) => {
+        console.log("Once a promise rejected on any of them.")
+        console.log(err) // Returns 'Connection Timeout :('
+    })
